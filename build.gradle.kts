3bbb317be46114dd81cd2e7fd9b7a950875d4498
offbyone <offbyone@disroot.org>
2026-06-03 13:57:45 +0200

    add missing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,22 @@ group = "com.github.selfcrafted"
 version = "0.1.0-SNAPSHOT"
 
 repositories {
+    mavenCentral()
     maven {
         name = "papermc"
         url = uri("https://repo.papermc.io/repository/maven-public/")
     }
+    maven {
+        name = "viaversion"
+        url = uri("https://repo.viaversion.com/everything/")
+    }
 }
 
 dependencies {
+    compileOnly(libs.viaversion.api)
     compileOnly(libs.velocity.api)
     annotationProcessor(libs.velocity.api)
+    implementation(libs.paho.mqtt)
 }
 
 sourceSets {
